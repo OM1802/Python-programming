8a7545f474ce2e3e304ef5d4aaa13847e7ebbb67
OM_JAISWAL <omjaiswal1802@gmail.com>
2024-02-04 22:49:20 +0530

    Update wiki.py

--- wiki.py
+++ wiki.py
@@ -1,7 +1,7 @@
 #PLEASE MAKE SURE THAT YOU HAVE INSTALLED THE "pyttsx3 and wikipedia module" BEFORE RUNNING THIS PROGRAM
 
-import pyttsx3
-import wikipedia
+import pyttsx3 #importing pyttsx3 to conver the output into audio
+import wikipedia #importting wikipedia to search. The information given are updated to January, 2023
 
 voice = pyttsx3.init()
 search_query = input("PLEASE TYPE THE WIKIPEDIA SEARCH TERM: ")
