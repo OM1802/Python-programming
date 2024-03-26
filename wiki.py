37f1f8e5e48697512a31f51ea003a613aa3c2dd3
OM_JAISWAL <omjaiswal1802@gmail.com>
2024-03-26 22:02:58 +0530

    Update wiki.py

--- wiki.py
+++ wiki.py
@@ -1,4 +1,5 @@
 #PLEASE MAKE SURE THAT YOU HAVE INSTALLED THE "pyttsx3 and wikipedia module" BEFORE RUNNING THIS PROGRAM
+# This program take input of any query and give the result according to the Wikipedia update till Jan 2020
 
 import pyttsx3 #importing pyttsx3 to conver the output into audio
 import wikipedia #importting wikipedia to search. The information given are updated to January, 2023
