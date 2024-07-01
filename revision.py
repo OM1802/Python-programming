d6a8a6ef2840c5885bfd9e57ff26cf88d07bd011
OM_JAISWAL <omjaiswal1802@gmail.com>
2024-07-01 11:19:15 +0530

    Update revision.py

--- revision.py
+++ revision.py
@@ -248,3 +248,20 @@ def insert(self,prev_node,NewVal):
                   last =last.next
                   last.next =NewNode
                   NewNode.prev =last
+
+#taking names as arguments by a function named teams() randomly divide all names in team of two, make sure that the number of names taken as input is even
+import random
+
+def teams(names):
+    if len(names) % 2 != 0:
+        raise ValueError("The number of names must be even.")
+    random.shuffle(names)
+    teams = []
+    for i in range(0, len(names), 2):
+        teams.append((names[i], names[i+1]))
+    return teams
+    
+names = ["OM", "HARSH", "UDIT", "SANJEEV", "SHIWADITYA", "ANUBHAV"]
+result = teams(names)
+for i, team in enumerate(result, 1):
+    print(f"Team {i}: {team[0]} and {team[1]}")
